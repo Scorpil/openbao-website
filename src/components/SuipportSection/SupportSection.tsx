b3ac61a6ecc4d604dae2b491410dd19746c5ce3b
robb <gxrobb@gmail.com>
2023-12-15 22:12:30 -0500

    support section done
    Signed-off-by: robb <gxrobb@gmail.com>

--- src/components/SuipportSection/SupportSection.tsx
+++ src/components/SuipportSection/SupportSection.tsx
@@ -6,11 +6,19 @@ import { logoCardList } from '@/constants/logoCardList'
 export default function SupportSection() {
   return (
     <div className={styles.support__section}>
-      <h2 className="main__title__h2">Who supports us?</h2>
+      <h2 className={`${styles.support__section__title} main__title__h2`}>
+        Who supports us?
+      </h2>
 
       <p>
-        One <span>2145</span> Two <span>5898</span> Three <span>12</span> Blue{' '}
-        <span>95</span>
+        One{' '}
+        <span className={styles.support__section__text__highlights}>2145</span>{' '}
+        Two{' '}
+        <span className={styles.support__section__text__highlights}>5898</span>{' '}
+        Three{' '}
+        <span className={styles.support__section__text__highlights}>12</span>{' '}
+        Blue{' '}
+        <span className={styles.support__section__text__highlights}>95</span>
       </p>
 
       <LogoCardGroup cards={logoCardList} />
